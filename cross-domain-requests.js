eff591d2f175cacaef2b3b00ab6342d10dbebd0b
Jeffrey Way <jeffrey@jeffrey-way.com>
2010-03-17 02:51:58 -0500

    Various improvements.

--- cross-domain-requests.js
+++ cross-domain-requests.js
@@ -1,9 +1,8 @@
 /*
  * Create a YQL query, strip all script tags, and insert into the parent element.
- * DON'T USE THIS YET. IT'S FOR A QUICK TIP ON NETTUTS, AND WILL BE IMPROVED DRASTICALLY.
 */ 
 
-function requestCrossDomain(site) {
+function requestCrossDomain(site, callback) {
 	if ( !site ) {
 		alert('No site passed');
 		return false;
@@ -11,26 +10,17 @@ function requestCrossDomain(site) {
 	
 	var that = this;
 	
-	// EXPAND THIS! SUCKS RIGHT NOW.
-	function isExternal(url) { return url.match('https?://|www\.'); }
-	
-	if ( isExternal(site) ) {
-		// cross domain request
-		yql = 'http://query.yahooapis.com/v1/public/yql?q=' + encodeURIComponent('select * from html where url="' + site + '"') + '&format=xml&callback=?';
+	yql = 'http://query.yahooapis.com/v1/public/yql?q=' + encodeURIComponent('select * from html where url="' + site + '"') + '&format=xml&callback=?';
 
-		$.getJSON(yql, function(data) {
-			if ( data.results[0] ) {
-				data = data.results[0].replace(/<script[^>]*>[\s\S]*?<\/script>/g,'');
-				$(that).parent().html(data);
-			}
+	$.getJSON(yql, function(data) {
+		if ( data.results[0] ) {
+			data = data.results[0].replace(/<script[^>]*>[\s\S]*?<\/script>/g,'');
 			
-			else throw new Error('Nothing returned from getJSON.');
-		});
-	}
+			if ( typeof callback === 'function' ) {
+				callback(data);
+			}
+		}
 		
-	else {
-		// local request. 
-		$(that).parent().load(site);
-	}
-	
+		else throw new Error('Nothing returned from getJSON.');
+	});	
 }
