255b61b3effa533464705ef0a9f3e201954559f5
Jeffrey Way <jeffrey@jeffrey-way.com>
2010-03-17 23:28:51 -0500

    Added fix for getJSON to prevent cache-busting with YQL

--- cross-domain-requests.js
+++ cross-domain-requests.js
@@ -1,24 +1,33 @@
-/*
- * Create a YQL query, strip all script tags, and insert into the parent element.
-*/ 
-
+// Accepts a url and a callback function to run.
 function requestCrossDomain( site, callback ) {
+	
+	// If no url was passed, exit.
 	if ( !site ) {
-		alert('No site passed');
+		alert('No site was passed.');
 		return false;
 	}
 	
-	yql = 'http://query.yahooapis.com/v1/public/yql?q=' + encodeURIComponent('select * from html where url="' + site + '"') + '&format=xml&callback=?';
-
-	$.getJSON(yql, function(data) {
-		if ( data.results[0] ) {
-			data = data.results[0].replace(/<script[^>]*>[\s\S]*?<\/script>/g,'');
-			
-			if ( typeof callback === 'function' ) {
-				callback(data);
-			}
-		}
+	// Take the provided url, and add it to a YQL query. Make sure you encode it!
+	var yql = 'http://query.yahooapis.com/v1/public/yql?q=' + encodeURIComponent('select * from html where url="' + site + '"') + '&format=xml&callback=?';
+	
+	// Request that YSQL string, and run a callback function.
+	// Pass a defined function to prevent cache-busting.
+	$.getJSON( yql, cbFunc );
+	
+	function cbFunc(data) {
+	// If we have something to work with...
+	if ( data.results[0] ) {
+		// Strip out all script tags, for security reasons.
+		// BE VERY CAREFUL. This helps, but we should do more. 
+		data = data.results[0].replace(/&lt;script[^>]*>[\s\S]*?&lt;\/script>/, '');
 		
-		else throw new Error('Nothing returned from getJSON.');
-	});	
+		// If the user passed a callback, and it
+		// is a function, call it, and send through the data var.
+		if ( typeof callback === 'function') {
+			callback(data);
+		}
+	}
+	// Else, Maybe we requested a site that doesn't exist, and nothing returned.
+	else throw new Error('Nothing returned from getJSON.');
+	}
 }
